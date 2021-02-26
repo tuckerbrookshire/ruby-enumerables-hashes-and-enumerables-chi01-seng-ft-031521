eaa2c94acfc7287589e4b6831f681dafe39bd24f
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 17:30:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,7 +4,6 @@
 #   "Amanda" => 27
 # }
 
-  def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
+def happy_birthday(birthday_kids)
+  # add your code snippet here!
+end
